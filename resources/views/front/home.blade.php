5db652dfa2b562f109f714d63ea7a521d38669e2
Larapeak <admin@larapeak.com>
2021-04-30 15:50:13 +0200

    home page design

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,3 +1,35 @@
 <x-app-layout title="okeeeee">
-    home
+    <div class="max-w-6xl mx-auto">
+        <div class="mb-12 sm:mb-24">
+            <h1 class="text-5xl font-bold">
+                Hi There 👋!
+            </h1>
+            <p class="mt-3 text-lg">
+                Welcome on the <span class="font-medium text-blue-600 underline">Larapeak</span> blog! <br>
+                On this blog you can find tutorials about Laravel, Tailwindcss and more!
+            </p>
+        </div>
+
+        <div class="grid grid-cols-1 gap-16 sm:grid-cols-2">
+            <div>
+                <div class="h-3 bg-gradient-to-r from-green-300 to-indigo-400 rounded-t-md"></div>
+                <div class="my-3">
+                    <p class="mb-3 text-sm font-semibold text-gray-400">
+                        Laravel - 30-04-2021
+                    </p>
+
+                    <h2 class="mb-3 text-3xl font-semibold">
+                        Blog title
+                    </h2>
+                    <p class="mb-3">
+                        description
+                    </p>
+
+                    <a href="/" class="font-bold text-blue-600">
+                        Read article
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    tailwindcss: {
+        jit: true
+    },
+
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -9,19 +13,21 @@ module.exports = {
         './resources/js/**/*.vue',
     ],
 
+    darkMode: false, // media or class
+
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
+        fontFamily: {
+            sans: ['Inter var', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
         },
+        extend: {},
     },
 
     variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
+        extend: {},
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'), 
+        require('@tailwindcss/typography')
+    ],
 };
