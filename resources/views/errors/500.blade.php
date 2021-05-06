d6d75e46f095cab4f3a7659ddf10d4d2964184ef
Larapeak <admin@larapeak.com>
2021-05-06 13:48:16 +0200

    custom error pages

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('front.layouts.error', [
+    'title' => 'Server error',
+    'errorCode' => '500',
+    'homeLink' => false,
+])
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,5 @@
+@extends('front.layouts.error', [
+    'title' => 'Be right back',
+    'errorCode' => '503',
+    'homeLink' => false,
+])
