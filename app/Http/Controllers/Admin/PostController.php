0252414071a9a69931a6d4511b0523648e89b5f4
Larapeak <admin@larapeak.com>
2021-05-06 22:26:49 +0200

    basic table design

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Post;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -10,7 +12,11 @@ class PostController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard');
+        $posts = Post::recent()->get();
+
+        return Inertia::render('Dashboard', [
+            'posts' => $posts,
+        ]);
     }
 
     public function create()
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,7 +8,42 @@
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                <div class="overflow-x-auto bg-white rounded-md shadow">
+                    <table class="w-full whitespace-nowrap">
+                        <tr class="font-bold text-left">
+                            <th class="px-6 py-4">#</th>
+                            <th class="px-6 py-4">Title</th>
+                            <th class="px-6 py-4">Status</th>
+                        </tr>
+                        <tr v-for="post in posts" :key="post.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+                            <td class="border-t">
+                                <inertia-link class="flex items-center px-6 py-4" :href="route('dashboard')">
+                                    {{ post.id }}
+                                </inertia-link>
+                            </td>
+                            <td class="border-t">
+                                <inertia-link class="flex items-center px-6 py-4" :href="route('dashboard')">
+                                    {{ post.title }}
+                                </inertia-link>
+                            </td>
+                            <td class="border-t">
+                                <inertia-link class="flex items-center px-6 py-4" :href="route('dashboard')">
+                                    <div v-if="post.published == 1" class="flex items-center px-2 py-1 text-xs text-green-900 bg-green-200 rounded-full">
+                                        <div class="w-2 h-2 mr-1 bg-green-500 rounded-full"></div>
+                                        Online
+                                    </div>
+                                    <div v-if="post.published == 1" class="flex items-center px-2 py-1 text-xs text-yellow-900 bg-yellow-200 rounded-full">
+                                        <div class="w-2 h-2 mr-1 bg-yellow-500 rounded-full"></div>
+                                        Draft
+                                    </div>
+                                    <div v-if="post.published == 1" class="flex items-center px-2 py-1 text-xs text-red-900 bg-red-200 rounded-full">
+                                        <div class="w-2 h-2 mr-1 bg-red-500 rounded-full"></div>
+                                        Deleted
+                                    </div>
+                                </inertia-link>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
@@ -21,6 +56,9 @@
     export default {
         components: {
             AppLayout,
+        },
+        props: {
+            posts: Array,
         },
     }
 </script>
