c13bfeaa1805e423dd5f58130137f58770ddaf62
Larapeak <admin@larapeak.com>
2021-05-18 21:52:31 +0200

    fix syntax error

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -39,6 +39,6 @@ class GenerateSitemap extends Command
     public function handle()
     {
         SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'))
+            ->writeToFile(public_path('sitemap.xml'));
     }
 }
