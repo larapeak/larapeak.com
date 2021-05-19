882e0fd1eecf6b58e5c0d00d12921f89918d6a70
Larapeak <admin@larapeak.com>
2021-05-19 11:52:39 +0200

    update error messages

--- resources/views/front/layouts/error.blade.php
+++ resources/views/front/layouts/error.blade.php
@@ -8,7 +8,12 @@
         <div class="leading-none text-center text-black md:text-left">
             <h1 class="mb-2 text-5xl font-extrabold">{{ $errorCode }}</h1>
             <p class="text-xl text-gray-900">
-                Hello, is it me you're looking for?
+                @isset($title)
+                    {{ $title }}
+                @else
+                    Hello, is it me you're looking for?
+                @endisset
+
                 @if($homeLink ?? false)
                     <a href="{{ url('/') }}" class="font-bold underline transition duration-300 hover:text-blue-600">Go home</a>
                 @endif
