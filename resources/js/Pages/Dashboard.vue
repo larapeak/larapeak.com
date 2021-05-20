97c0588bb2155c59a562b98f9cda61d892345304
Larapeak <admin@larapeak.com>
2021-05-20 12:20:00 +0200

    add link to dashboard cards

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,7 +9,7 @@
         <div class="px-4 py-12 sm:px-0">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="grid grid-cols-2 gap-16">
-                    <div class="relative p-6 overflow-hidden text-white bg-green-500 rounded-md">
+                    <inertia-link :href="route('users.index')" class="relative p-6 overflow-hidden text-white transition duration-300 transform bg-green-500 rounded-md hover:scale-105">
                         <span class="text-5xl font-black">{{ users_count }}</span>
                         <h2 class="text-3xl font-extrabold">User(s)</h2>
 
@@ -18,9 +18,9 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                             </svg>
                         </div>
-                    </div>
+                    </inertia-link>
 
-                    <div class="relative p-6 text-white bg-blue-500 rounded-md">
+                    <inertia-link :href="route('posts.index')" class="relative p-6 overflow-hidden text-white transition duration-300 transform bg-blue-500 rounded-md hover:scale-105">
                         <span class="text-5xl font-black">{{ posts_count }}</span>
                         <h2 class="text-3xl font-extrabold">Posts</h2>
 
@@ -29,7 +29,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                             </svg>
                         </div>
-                    </div>
+                    </inertia-link>
                 </div>
             </div>
         </div>
