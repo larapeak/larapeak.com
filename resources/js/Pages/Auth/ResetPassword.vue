f7d86386d7732dc83c0920aa05e88530de153206
Larapeak <admin@larapeak.com>
2021-05-21 16:29:02 +0200

    remove AuthenticationCard

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,34 +1,44 @@
 <template>
-    <jet-authentication-card>
-        <jet-validation-errors class="mb-4" />
+    <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+        <div>
+            <inertia-link :href="'/'">
+                <svg class="w-16 h-16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
+                    <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+                </svg>
+            </inertia-link>
+        </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="block w-full mt-1" v-model="form.email" required autofocus />
-            </div>
+        <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
+            <jet-validation-errors class="mb-4" />
 
-            <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="block w-full mt-1" v-model="form.password" required autocomplete="new-password" />
-            </div>
+            <form @submit.prevent="submit">
+                <div>
+                    <jet-label for="email" value="Email" />
+                    <jet-input id="email" type="email" class="block w-full mt-1" v-model="form.email" required autofocus />
+                </div>
 
-            <div class="mt-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="block w-full mt-1" v-model="form.password_confirmation" required autocomplete="new-password" />
-            </div>
+                <div class="mt-4">
+                    <jet-label for="password" value="Password" />
+                    <jet-input id="password" type="password" class="block w-full mt-1" v-model="form.password" required autocomplete="new-password" />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
-                </jet-button>
-            </div>
-        </form>
-    </jet-authentication-card>
+                <div class="mt-4">
+                    <jet-label for="password_confirmation" value="Confirm Password" />
+                    <jet-input id="password_confirmation" type="password" class="block w-full mt-1" v-model="form.password_confirmation" required autocomplete="new-password" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Reset Password
+                    </jet-button>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
-    import JetAuthenticationCard from '@/Shared/AuthenticationCard'
     import JetButton from '@/Shared/Buttons/Button'
     import JetInput from '@/Shared/FormFields/Input'
     import JetLabel from '@/Shared/FormFields/Label'
@@ -36,7 +46,6 @@
 
     export default {
         components: {
-            JetAuthenticationCard,
             JetButton,
             JetInput,
             JetLabel,
