bfb13e6ff1f3637f828d82c698ac450cbd83e995
Larapeak <admin@larapeak.com>
2021-05-21 16:30:09 +0200

    move .NavLinks

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -141,8 +141,8 @@
     import JetBanner from '@/Shared/Messages/Banner'
     import JetDropdown from '@/Shared//Dropdown/Dropdown'
     import JetDropdownLink from '@/Shared/Dropdown/DropdownLink'
-    import JetNavLink from '@/Shared/Navigation/NavLink'
-    import JetResponsiveNavLink from '@/Shared/Navigation/ResponsiveNavLink'
+    import JetNavLink from './NavLinks/NavLink'
+    import JetResponsiveNavLink from './NavLinks/ResponsiveNavLink'
 
     export default {
         components: {
