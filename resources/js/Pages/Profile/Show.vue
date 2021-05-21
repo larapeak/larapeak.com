9e94c594b92451b7bebd9385601b83fd4d30fb7b
Larapeak <admin@larapeak.com>
2021-05-21 17:08:27 +0200

    remove unused views

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -20,19 +20,7 @@
                     <jet-section-border />
                 </div>
 
-                <div v-if="$page.props.jetstream.canManageTwoFactorAuthentication">
-                    <two-factor-authentication-form class="mt-10 sm:mt-0" />
-
-                    <jet-section-border />
-                </div>
-
                 <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
-
-                <template v-if="$page.props.jetstream.hasAccountDeletionFeatures">
-                    <jet-section-border />
-
-                    <delete-user-form class="mt-10 sm:mt-0" />
-                </template>
             </div>
         </div>
     </app-layout>
@@ -40,10 +28,8 @@
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
-    import DeleteUserForm from './DeleteUserForm'
     import JetSectionBorder from '@/Shared/FormFields/Section/SectionBorder'
     import LogoutOtherBrowserSessionsForm from './LogoutOtherBrowserSessionsForm'
-    import TwoFactorAuthenticationForm from './TwoFactorAuthenticationForm'
     import UpdatePasswordForm from './UpdatePasswordForm'
     import UpdateProfileInformationForm from './UpdateProfileInformationForm'
 
@@ -52,10 +38,8 @@
 
         components: {
             AppLayout,
-            DeleteUserForm,
             JetSectionBorder,
             LogoutOtherBrowserSessionsForm,
-            TwoFactorAuthenticationForm,
             UpdatePasswordForm,
             UpdateProfileInformationForm,
         },
